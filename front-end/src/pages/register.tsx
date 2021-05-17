7eee15e1b70021096bf4a29e6b587324515ce7ae
PadoSensei <padraigo.donoghue@gmail.com>
2021-05-17 18:26:46 +0100

    Add Chakra & Register Page

---
+++ front-end/src/pages/register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface registerProps {
+
+}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return <div>Register Page</div>
+}
+
+export default Register
