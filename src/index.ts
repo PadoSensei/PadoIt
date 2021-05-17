5ac6ca5691cd12c30e3ad90218f502806fa3eb1a
PadoSensei <padraigo.donoghue@gmail.com>
2021-05-17 15:45:17 +0100

    Add Redis and Login with cookie

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,62 @@
-import 'reflect-metadata';
-import { MikroORM } from '@mikro-orm/core';
-import { __prod__ } from './constants'
-import microConfig from './mikro-orm.config'
-import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
-import { buildSchema } from 'type-graphql'
-import { HelloResolver } from './resolvers/hello';
-import { PostResolver } from './resolvers/post';
-import { UserResolver } from './resolvers/user';
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-  await orm.getMigrator().up()
-  
+  await orm.getMigrator().up();
+
   const app = express();
-  
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        //disableTTL: true,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365, // lasts one year
+        httpOnly: true,
+        sameSite: "lax", //csrf
+        secure: __prod__, //cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "creggan_21",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
-      validate: false
+      validate: false,
     }),
-    context: () => ({ em: orm.em })
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app })
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
-    console.log('server started on localhost: 4000')
-  })
-
-}
+    console.log("server started on localhost: 4000");
+  });
+};
 
 main().catch((err) => {
-  console.log(err)
-})
+  console.log(err);
+});
